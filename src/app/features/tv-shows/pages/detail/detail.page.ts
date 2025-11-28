5436a867ef7b2158e5c4cdfb1d03b6a8ca34b172
Airan <airan_234@hotmail.com>
2025-11-28 19:35:41 +0000

    prepared detail page

--- src/app/features/tv-shows/pages/detail/detail.page.ts
+++ src/app/features/tv-shows/pages/detail/detail.page.ts
@@ -6,8 +6,19 @@ import {
   IonHeader,
   IonTitle,
   IonToolbar,
+  IonButton,
+  IonIcon,
+  IonButtons,
+  IonLabel,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonNote,
+  IonChip,
+  IonFooter,
 } from '@ionic/angular/standalone';
-import { TvShowsService } from '../../services/tv-shows.service';
+import { NavController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import {
   distinctUntilChanged,
@@ -17,6 +28,13 @@ import {
   switchMap,
   tap,
 } from 'rxjs';
+import { addIcons } from 'ionicons';
+import { arrowBack, star } from 'ionicons/icons';
+
+import { TvShowsService } from '../../services/tv-shows.service';
+import { TVShowsModel } from '../../models';
+import { SharedPipes } from '../../../../shared/pipes/shared.pipes';
+import { SharedComponents } from '../../../../shared/components/shared.components';
 
 @Component({
   selector: 'app-detail',
@@ -24,31 +42,50 @@ import {
   styleUrls: ['./detail.page.scss'],
   standalone: true,
   imports: [
+    IonFooter,
+    IonChip,
+    IonNote,
+    IonItem,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonLabel,
+    IonButtons,
+    IonIcon,
+    IonButton,
     IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
     CommonModule,
     FormsModule,
+    ...SharedPipes,
+    ...SharedComponents,
   ],
 })
 export class DetailPage implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
   private route = inject(ActivatedRoute);
+  private navCtrl = inject(NavController);
   tvShowsService = inject(TvShowsService);
+  tvShow = signal<TVShowsModel | null>(null);
   isLoading = signal(false);
-  hasError = signal('');
+  errorMessage = signal<string | null>(null);
   showId = signal(0);
 
+  constructor() {
+    addIcons({ star, arrowBack });
+  }
+
   ngOnInit(): void {
-    this.getTvShow();
+    this.loadTvShow();
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  private getTvShow() {
+  loadTvShow() {
     const subs = this.route.paramMap
       .pipe(
         map((params) => params.get('id')),
@@ -56,7 +93,7 @@ export class DetailPage implements OnInit, OnDestroy {
         filter((id) => id !== null && id !== ''),
         tap(() => {
           this.isLoading.set(true);
-          this.hasError.set('');
+          this.errorMessage.set('');
         }),
         switchMap((id) => {
           return this.tvShowsService.getTvShow(Number(id));
@@ -64,15 +101,22 @@ export class DetailPage implements OnInit, OnDestroy {
       )
       .subscribe({
         next: (tvShow) => {
-          console.log({ tvShow });
+          this.tvShow.set(tvShow);
           this.isLoading.set(false);
         },
         error: (error: Error) => {
-          this.hasError.set(error.message);
+          this.errorMessage.set(error.message);
           this.isLoading.set(false);
         },
       });
 
     this.subscription.add(subs);
+  }
+
+  favorito() {
+    throw new Error('Method not implemented.');
+  }
+  goBack() {
+    this.navCtrl.back(); // Vuelve a la página anterior en la pila de navegación
   }
 }
